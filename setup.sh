8277bd08f7ae86caa3a6e4d869c63ba5fd59fb0c
benaryorg <binary@benary.org>
2016-11-16 01:59:19 +0100

    hidden files are not copied
    Signed-off-by: benaryorg <binary@benary.org>

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ pushd $(dirname ${0}) > /dev/null
 FILES=$(pwd -P)
 echo symlinking to ${FILES}
 
-xargs -n 1 -I "{}" ln -s ${FILES}/"{}" ~/."{}" <<(ls ${FILES} | egrep -v "^$(basename ${0})$")
+xargs -n 1 -I "{}" ln -s ${FILES}/"{}" ~/."{}" <<(ls ${FILES} | egrep -v "^($(basename ${0})|\..+)$")
 
 vim +PlugInstall
 
