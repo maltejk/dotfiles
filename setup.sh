da64ce0873cd18e0917525ff259ba92bd032816b
benaryorg <binary@benary.org>
2017-08-06 02:02:56 +0200

    setup.sh: cleanup
    Signed-off-by: benaryorg <binary@benary.org>

--- setup.sh
+++ setup.sh
@@ -3,11 +3,16 @@
 git submodule init
 git submodule update
 
-pushd $(dirname ${0}) > /dev/null
-FILES=$(pwd -P)
-echo symlinking to ${FILES}
+pushd "$(dirname "${0}")" > /dev/null
+FILES="$(pwd -P)"
+echo symlinking to "${FILES}"
 
-xargs -n 1 -I "{}" ln -s ${FILES}/"{}" ~/."{}" <<(ls ${FILES} | egrep -v "^($(basename ${0})|\..+)$")
+ls "${FILES}" \
+	| egrep -v "^($(basename ${0})|\..+)$" \
+	| while read -r line;do
+		! test -e "${HOME}/.${line}" \
+			&& ln -s "${FILES}/${line}" "${HOME}/.${line}"
+	done
 
 vim +PlugInstall
 
