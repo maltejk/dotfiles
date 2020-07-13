3ed4bff434370c0397aafc00374640d4ebfdff8b
Malte Jan Kaffenberger <malte@maltejk.de>
2020-07-13 18:45:37 +0200

    remove traces of zsh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,18 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 
-git submodule init
-git submodule update
-
-pushd "$(dirname "${0}")" > /dev/null
-FILES="$(pwd -P)"
-echo symlinking to "${FILES}"
-
-ls "${FILES}" \
-	| egrep -v "^($(basename ${0})|\..+)$" \
-	| while read -r line;do
-		! test -e "${HOME}/.${line}" \
-			&& ln -s "${FILES}/${line}" "${HOME}/.${line}"
-	done
+#git submodule init
+#git submodule update
+#
+#pushd "$(dirname "${0}")" > /dev/null
+#FILES="$(pwd -P)"
+#echo symlinking to "${FILES}"
+#
+#ls "${FILES}" \
+#	| egrep -v "^($(basename ${0})|\..+)$" \
+#	| while read -r line;do
+#		! test -e "${HOME}/.${line}" \
+#			&& ln -s "${FILES}/${line}" "${HOME}/.${line}"
+#	done
 
 vim +PlugInstall +PlugUpdate +q +q
 
